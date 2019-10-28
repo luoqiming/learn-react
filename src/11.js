2e67ca0f29526d703f589bf23f6e8aceeda0a8d0
unknown <619165002@qq.com>
2019-10-28 18:00:43 +0800

    React-router跳转功能

--- src/11.js
+++ src/11.js
@@ -1,4 +1,6 @@
 import React from "react"
+// 编程化的导航跳转需要引入withRouter模块
+import { withRouter } from "react-router-dom";
 // 为class组件添加refs，步骤和为dom元素添加refs是相同的
 class Example extends React.Component {
     constructor(props) {
@@ -9,16 +11,22 @@ class Example extends React.Component {
     }
     render() {
         return (
-            <p style={{ border: "1px solid #000000" }}>{this.state.txt}</p>
+            <div>
+                <p style={{ border: "1px solid #000000" }}>{this.state.txt}</p>
+                <span onClick={this.go.bind(this)}>点我进行跳转</span>
+            </div>
         )
     }
     sayHi() {
         alert(this.state.txt);
     }
+    go() {
+        this.props.history.push("page2");
+        // console.log(this.props.history);
+    }
 }
 
-
-export default class Box extends React.Component {
+class Box extends React.Component {
     constructor(props) {
         super(props);
         this.example = React.createRef();
@@ -28,11 +36,16 @@ export default class Box extends React.Component {
         this.example.current.sayHi();
     }
     render() {
+        const history = this.props.history;
         return (<h1>
-            <Example ref={this.example}></Example>
+            {/* 需要传递history进去，不然子组件的跳转方法报undefined */}
+            <Example ref={this.example} history={history}></Example>
         </h1>)
     }
 }
+
+
+export default withRouter(Box)
 
 
 
